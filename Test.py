7340f0c8c274fc0f02e58d353b4402d51170789c
Keti Meladze <keti.meladze@aversi.ge>
2026-09-17 10:02:43 +0400

    First commit

---
+++ Test.py
@@ -0,0 +1,30 @@
+# #############print("Hellow World!")
+# ##############print ("Hello")
+# ###############print ("გამარჯობა ქეთი")
+# ###################
+# # print (10.5)
+# # print (10)
+# # print (10 + 5)
+# # print (10//3)
+# # print (10 % 3)
+# # print (5 / 2)
+# # print (5**3)
+# # print ("keti")
+# # print (type("keti"))
+# # print ("keti, nutsa")
+# ######################
+# # print (True)
+# # print (False)
+# # print (10 > 5)
+# # print (10 == 5)
+# # print (10 !=5)
+num1 = 5
+num2 = 2
+print (num1 + num2)
+count = 3
+print (count)
+conut = count -1
+count -= 1
+print (count)
+num1 = int (input(" enter a nim:"))
+num2 = int (input )
